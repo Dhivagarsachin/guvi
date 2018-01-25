4c8a02a7dcd3199256d039499c27d152bcc27b50
Dhivagarsachin <35727666+Dhivagarsachin@users.noreply.github.com>
2018-01-25 16:51:05 +0530

    Update LEAP.C

--- LEAP.C
+++ LEAP.C
@@ -10,14 +10,14 @@ int main()
         {
             // year is divisible by 400, hence the year is a leap year
             if ( year%400 == 0)
-                printf("%d is  leap year.", year);
+                printf("%d is B leap year.", year);
             else
-                printf("%d is not  leap year.", year);
+                printf("%d is not B  leap year.", year);
         }
         else
-            printf("%d is leap year.", year );
+            printf("%d is leap B year.", year );
     }
     else
-        printf("%d is not  leap year.", year);  
+        printf("%d is not  leap B year.", year);  
     return 0;
 }
