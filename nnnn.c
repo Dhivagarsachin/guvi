a6ec098f4792697305862453c1edb75c80309825
Dhivagarsachin <35727666+Dhivagarsachin@users.noreply.github.com>
2018-03-01 15:50:18 +0530

    Create nnnn.c

---
+++ nnnn.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+main()
+{
+   int y = 1;
+ 
+   if ( y== 1 )
+      printf("y is equal to one.\n");
+   else
+      printf("For comparison use '==' as '=' is the assignment operator.\n");
+ 
+   return 0;
+}
